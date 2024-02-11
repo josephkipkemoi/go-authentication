16c533bf937f84c72142845806dd2193c149c17e
jkemboe <jkemboe@gmail.com>
2024-02-11 18:43:07 +0300

    Access Proxy Set

--- database/user.go
+++ database/user.go
@@ -23,8 +23,8 @@ func (u User) SaveUser(db *sql.DB) (User, error) {
 
 	num := strconv.Itoa(u.PhoneNumber)
 	// Confirm number has set number of digits (9) before saving
-	if len(num) != 9 {
-		return User{}, errors.New("invalid phone number format. Expects 9 digits.")
+	if len(num) != 12 {
+		return User{}, errors.New("invalid phone number format. Expects 12 digits.")
 	}
 	// Add country prefix to phonenumber, receive 700545727 save 254700545727
 	n, err := strconv.Atoi("254" + num)
@@ -78,7 +78,8 @@ func (u User) GetUsers(db *sql.DB) {
 
 func (u User) AuthenticateUser(phone int, password string, db *sql.DB) (User, bool) {
 	var num int
-	if err := db.QueryRow("SELECT phone_number FROM users WHERE phone_number = ? AND password = ?", phone, password).Scan(&num); err != nil {
+	selectQuery := `SELECT phone_number FROM users WHERE phone_number = ? AND password = ?`
+	if err := db.QueryRow(selectQuery, phone, password).Scan(&num); err != nil {
 		return User{}, false
 	}
 	return User{PhoneNumber: num}, true
