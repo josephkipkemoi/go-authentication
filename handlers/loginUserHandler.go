c5c71cfd121bce0f1d677646f047154576d8094a
jkemboe <jkemboe@gmail.com>
2024-02-18 00:06:53 +0300

    Ready for deployment

--- handlers/loginUserHandler.go
+++ handlers/loginUserHandler.go
@@ -15,6 +15,7 @@ type Login struct {
 }
 
 func LoginUserHandler(c *gin.Context) {
+	c.Request.Header.Add("Access-Control-Allow-Origin", "https://applea.onrender.com")
 	i := Login{}
 	u := database.User{}
 
