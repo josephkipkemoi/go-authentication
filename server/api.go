d6e60c869b3ca74421bc9c1c234aee2e7f3cc582
jkemboe <jkemboe@gmail.com>
2024-02-17 23:16:51 +0300

    Backend URL

--- server/api.go
+++ server/api.go
@@ -2,12 +2,22 @@ package server
 
 import (
 	"jk/go-sportsapp/handlers"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func ConnectServerRouter() *gin.Engine {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://findmyiphone.vercel.app"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	r.Use(SetHeaders())
 
 	apiRoutes(r) // API endpoint URLs
