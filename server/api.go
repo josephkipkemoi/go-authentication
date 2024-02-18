0e9e0dd5617478803ff35d7ec5e23fe04077fb86
jkemboe <jkemboe@gmail.com>
2024-02-18 21:11:46 +0300

    Ready for deployment

--- server/api.go
+++ server/api.go
@@ -10,14 +10,6 @@ import (
 
 func ConnectServerRouter() *gin.Engine {
 	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"https://findmyiphone.vercel.app/"},
-		AllowMethods:     []string{"PUT", "PATCH", "GET", "DELETE", "POST"},
-		AllowHeaders:     []string{"Origin, Content-Type, Token, Accept, X-Requested-With, withCredentials, Access-Control-Allow-Origin"},
-		ExposeHeaders:    []string{"Origin, Content-Type, Token, Accept, X-Requested-With, Access-Control-Allow-Origin"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 
 	r.GET("/", handlers.LandingHandler)
 	r.POST("api/register", handlers.RegisterUserHandler)
@@ -30,12 +22,18 @@ func ConnectServerRouter() *gin.Engine {
 	r.GET("api/users/:user_id/balance", handlers.BalanceHandler)
 
 	// apiRoutes(r) // API endpoint URLs
-
+	r.Use(SetHeaders())
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"PUT", "PATCH", "GET", "DELETE", "POST"},
+		AllowHeaders:     []string{"Origin, Content-Type, Token, Accept, X-Requested-With, withCredentials, Access-Control-Allow-Origin"},
+		ExposeHeaders:    []string{"Origin, Content-Type, Token, Accept, X-Requested-With, Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	return r
 }
 
 // func apiRoutes(r *gin.Engine) {
-
-// 	// r.Use(SetHeaders())
 
 // }
